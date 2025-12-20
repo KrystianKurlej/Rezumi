7d8c0d27f3900defb0f9f53280ff4c8cce8704ee
Krystian Kurlej <235661@student.uek.krakow.pl>
2025-12-20 20:49:39 +0100

    feat: update Preview component to display experiences with improved layout

--- components/cv/preview.tsx
+++ components/cv/preview.tsx
@@ -4,15 +4,32 @@ import { useAppSelector } from "@/lib/hooks"
 
 export default function Preview() {
     const personal = useAppSelector(state => state.personal)
+    const experiences = useAppSelector(state => state.experiences.list)
 
     return(
-        <div className="p-8 bg-white shadow-sm">
-            <div className="text-sm text-gray-500">CV</div>
+        <div className="p-10 bg-white shadow-sm">
+            <div className="text-sm">CV</div>
             <div className="text-4xl font-semibold">{personal.firstName} {personal.lastName}</div>
-            <div className="py-1 border-b">
+            <div className="py-1">
                 <div>{personal.email}</div>
                 <div>{personal.phone}</div>
             </div>
+            {experiences.length > 0 && (
+                <div className="mt-4">
+                    <div className="text-xl font-semibold pb-1 mb-2 border-b border-gray-300">Experience</div>
+                    <ul>
+                        {experiences.map((experience) => (
+                            <li key={experience.id} className={"mb-2" + (experiences.length - 1 !== experiences.indexOf(experience) ? " border-b border-gray-200 pb-2" : "")}>
+                                <div className="flex gap-4">
+                                    <div className="grow"><span className="font-semibold">{experience.title}</span> - {experience.company}</div>
+                                    <div className="shrink-0">{experience.startDate} - {experience.endDate}</div>
+                                </div>
+                                <div className="text-sm">{experience.description}</div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </div>
     )
 }
