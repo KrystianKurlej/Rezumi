3c4c5f3cdbd9637af5a84c4bf0a09bae9177e9e9
Krystian Kurlej <235661@student.uek.krakow.pl>
2025-12-21 23:10:50 +0100

    feat: refactor ApplicationsTable layout for improved structure and readability

--- components/applications/ApplicationsTable.tsx
+++ components/applications/ApplicationsTable.tsx
@@ -8,7 +8,6 @@ import {
   flexRender,
   getCoreRowModel,
   getFilteredRowModel,
-  getPaginationRowModel,
   getSortedRowModel,
   useReactTable,
   type ColumnDef,
@@ -349,7 +348,6 @@ export default function ApplicationsTable() {
     onSortingChange: handleSortingChange,
     onColumnFiltersChange: setColumnFilters,
     getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
     getSortedRowModel: getSortedRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
     onColumnVisibilityChange: setColumnVisibility,
@@ -372,87 +370,69 @@ export default function ApplicationsTable() {
         </div>
       ) : (
         <>
-          <div className="overflow-hidden rounded-md border">
-            <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                    </TableHead>
-                  )
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  No results.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <div className="flex-1 space-x-2">
-            <ApplicationAddNewDialog
-                onAdd={handleAddApplication}
-                trigger={
-                <Button size="sm">
-                    <i className="bi bi-plus-lg"></i>
-                    Add New Application
-                </Button>
-                }
-            />
-        </div>
-        <div className="space-x-2">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            Previous
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            Next
-          </Button>
-        </div>
-          </div>
+            <div className="flex items-center justify-end space-x-2 pb-4">
+                <div className="flex-1 space-x-2">
+                    <ApplicationAddNewDialog
+                        onAdd={handleAddApplication}
+                        trigger={
+                        <Button>
+                            <i className="bi bi-plus-lg"></i>
+                            Add New Application
+                        </Button>
+                        }
+                    />
+                </div>
+            </div>
+            <div className="overflow-hidden rounded-md border">
+                <Table>
+                    <TableHeader>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                        <TableRow key={headerGroup.id}>
+                            {headerGroup.headers.map((header) => {
+                            return (
+                                <TableHead key={header.id}>
+                                {header.isPlaceholder
+                                    ? null
+                                    : flexRender(
+                                        header.column.columnDef.header,
+                                        header.getContext()
+                                    )}
+                                </TableHead>
+                            )
+                            })}
+                        </TableRow>
+                        ))}
+                    </TableHeader>
+                    <TableBody>
+                        {table.getRowModel().rows?.length ? (
+                        table.getRowModel().rows.map((row) => (
+                            <TableRow
+                            key={row.id}
+                            data-state={row.getIsSelected() && "selected"}
+                            >
+                            {row.getVisibleCells().map((cell) => (
+                                <TableCell key={cell.id}>
+                                {flexRender(
+                                    cell.column.columnDef.cell,
+                                    cell.getContext()
+                                )}
+                                </TableCell>
+                            ))}
+                            </TableRow>
+                        ))
+                        ) : (
+                        <TableRow>
+                            <TableCell
+                            colSpan={columns.length}
+                            className="h-24 text-center"
+                            >
+                            No results.
+                            </TableCell>
+                        </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
         </>
       )}
     </div>
