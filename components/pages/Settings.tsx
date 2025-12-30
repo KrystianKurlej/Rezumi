2f0324404f52a819199684d25eec01ec925c143e
Krystian Kurlej <235661@student.uek.krakow.pl>
2025-12-30 22:34:22 +0100

    feat: add functionality to remove all CV data with confirmation dialog

--- components/pages/Settings.tsx
+++ components/pages/Settings.tsx
@@ -406,6 +406,46 @@ export default function Settings() {
                         </DialogContent>
                     </Dialog>
                 </SettingsSection>
+                <Separator className="my-6" />
+                <SettingsSection
+                    title="Remove all data"
+                    description="If you want to start fresh, you can remove all your CV data from the application."
+                >
+                    <Dialog>
+                        <DialogTrigger asChild>
+                            <Button variant="outline" className="w-64">
+                                Remove all data
+                                <i className="bi bi-trash"></i>
+                            </Button>
+                        </DialogTrigger>
+                        <DialogContent>
+                            <DialogHeader>
+                                <DialogTitle>Remove All Data?</DialogTitle>
+                                <DialogDescription>
+                                    Are you sure you want to remove all your CV data? This action cannot be undone.
+                                </DialogDescription>
+                            </DialogHeader>
+                            <DialogFooter>
+                                <DialogClose asChild>
+                                    <Button variant="outline">
+                                        Cancel
+                                    </Button>
+                                </DialogClose>
+                                <DialogClose asChild>
+                                    <Button 
+                                        variant="destructive"
+                                        onClick={async () => {
+                                            await importDB('{"cvData":[],"templates":[],"settings":{}}');
+                                            window.location.reload();
+                                        }}
+                                    >
+                                        Remove all data
+                                    </Button>
+                                </DialogClose>
+                            </DialogFooter>
+                        </DialogContent>
+                    </Dialog>
+                </SettingsSection>
             </div>
         </ScrollArea>
     )
