ddb24ae2ca4204ef876b305a5ba37df5d402ce35
Krystian Kurlej <235661@student.uek.krakow.pl>
2026-01-01 17:12:19 +0100

    feat: add Tabs component and integrate into TemplateEditDialog for better organization

--- components/templates/TemplateEditDialog.tsx
+++ components/templates/TemplateEditDialog.tsx
@@ -11,7 +11,8 @@ import {
   DialogTrigger,
   DialogTitle,
   DialogFooter,
-  DialogClose
+  DialogClose,
+  DialogDescription
 } from "@/components/ui/dialog"
 import {
   Field,
@@ -22,6 +23,21 @@ import { Input } from "@/components/ui/input"
 import { useAppDispatch, useAppSelector } from "@/lib/hooks"
 import { setSelectedDesign } from "@/lib/slices/templatesSlice"
 import DesignForm from "@/components/templates/Design"
+import { Label } from "@/components/ui/label"
+import { Switch } from "@/components/ui/switch"
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+import { Textarea } from '../ui/textarea'
 
 interface TemplateEditDialogProps {
   template: DBTemplates
@@ -87,6 +103,7 @@ export function TemplateEditDialog({
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Edit <em>{template.name}</em></DialogTitle>
+          <DialogDescription>Templates let you create CV variations without duplicating your data. You can hide sections or override content for specific roles.</DialogDescription>
         </DialogHeader>
         <FieldGroup>
           <Field>
@@ -100,13 +117,65 @@ export function TemplateEditDialog({
               placeholder="e.g. Software Engineer CV"
             />
           </Field>
-          <Field>
-            <FieldLabel>
-              Select Design
-            </FieldLabel>
-            <DesignForm />
-          </Field>
         </FieldGroup>
+        <Tabs defaultValue="appearance">
+          <TabsList>
+            <TabsTrigger value="appearance">Appearance</TabsTrigger>
+            <TabsTrigger value="content">Content</TabsTrigger>
+          </TabsList>
+          <TabsContent value="appearance">
+            <FieldGroup>
+              <Field>
+                <DesignForm />
+              </Field>
+            </FieldGroup>
+          </TabsContent>
+          <TabsContent value="content">
+            <FieldGroup>
+              <Field>
+                <Accordion type="single" collapsible>
+                  <AccordionItem value="personal-information">
+                    <AccordionTrigger>Personal Information</AccordionTrigger>
+                    <AccordionContent>
+                      <div className='border p-3 rounded mb-1'>
+                        <div className="text-gray-600 mb-1">Profile picture</div>
+                        <div className="flex items-center space-x-2 py-1">
+                          <Switch id="profile-picture-show" checked />
+                          <Label htmlFor="profile-picture-show">Show section</Label>
+                        </div>
+                        <div className="flex items-center space-x-2 py-1">
+                          <Switch id="profile-picture-custom" />
+                          <Label htmlFor="profile-picture-custom">Use custom content</Label>
+                        </div>
+                      </div>
+                      <div className='border p-3 rounded'>
+                        <div className="text-gray-600 mb-1">About</div>
+                        <div className="flex items-center space-x-2 py-1">
+                          <Switch id="about-show" checked />
+                          <Label htmlFor="about-show">Show section</Label>
+                        </div>
+                        <div className="flex items-center space-x-2 py-1">
+                          <Switch id="about-custom" checked/>
+                          <Label htmlFor="about-custom">Use custom content</Label>
+                        </div>
+                        <Textarea className="mt-2" placeholder="Write something about yourself" />
+                      </div>
+                    </AccordionContent>
+                  </AccordionItem>
+                  <AccordionItem value="experience">
+                    <AccordionTrigger>Experience</AccordionTrigger>
+                    <AccordionContent>
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
+                <Button variant="outline" size="sm" className="mt-1">
+                  Reset all content customizations
+                  <i className="bi bi-arrow-clockwise ml-2"></i>
+                </Button>
+              </Field>
+            </FieldGroup>
+          </TabsContent>
+        </Tabs>
         <DialogFooter>
           <DialogClose asChild>
             <Button variant="outline">Cancel</Button>
