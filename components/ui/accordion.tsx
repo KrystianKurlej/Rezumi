e05ced5214db71212b490c67b17554d7e857f133
Krystian Kurlej <235661@student.uek.krakow.pl>
2026-01-10 23:40:04 +0100

    refactor: update dark theme colors for improved contrast and accessibility

--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -35,7 +35,7 @@ function AccordionTrigger({
       <AccordionPrimitive.Trigger
         data-slot="accordion-trigger"
         className={cn(
-          "cursor-pointer focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md my-1 p-3 text-left text-sm font-medium transition-all outline-none hover:bg-gray-50 dark:hover:bg-muted focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180",
+          "cursor-pointer focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md my-1 p-3 text-left text-sm font-medium transition-all outline-none hover:bg-gray-50 dark:hover:bg-gray-900 focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180",
           className
         )}
         {...props}
